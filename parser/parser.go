ea17bf14e55db89b54f91bffa1d66acc0d56797a
Martin Frlin <martin.frlin@gmail.com>
2017-01-10 11:33:47 +0100

    Fleshed out parser.

--- parser/parser.go
+++ parser/parser.go
@@ -3,27 +3,62 @@ package parser
 import (
 	"io/ioutil"
 	"encoding/json"
+	"strings"
+	"errors"
 )
 
 var calling_codes map[string]string
+var mno_identifiers map[string]map[string]string
 
 func init() {
-	file, _ := ioutil.ReadFile("./parser/resources/countries.json")
-	json.Unmarshal(file, &calling_codes)
+	countries, _ := ioutil.ReadFile("./parser/resources/countries.json")
+	json.Unmarshal(countries, &calling_codes)
+
+	mnos, _ := ioutil.ReadFile("./parser/resources/mnos.json")
+	json.Unmarshal(mnos, &mno_identifiers)
 }
 
 type MsisdnInfo struct {
 	country_code string
+	dialing_number string
+	mno_identifier string
+	subscriber_number string
 }
 
-func Parse_msisdn(msisdn string) (MsisdnInfo) {
-	var code string
-	for i := 1; i <= len(msisdn); i++ {
-		var ok bool
-		code, ok = calling_codes[msisdn[0:i]]
+func Parse_msisdn(msisdn string) (MsisdnInfo, error) {
+	start := find_msisdn_start(msisdn)
+	for i := start+1; i <= len(msisdn); i++ {
+		dialing_number := msisdn[start:i]
+		code, ok := calling_codes[dialing_number]
 		if ok {
-			break
+			subscriber_number := msisdn[i:]
+			return MsisdnInfo{country_code: code,
+				dialing_number: dialing_number,
+				mno_identifier: find_mno_identifier(code, subscriber_number),
+				subscriber_number: subscriber_number}, nil
 		}
 	}
-	return MsisdnInfo{country_code: code}
+	return MsisdnInfo{}, errors.New("Country code not found.")
+}
+
+func find_msisdn_start(msisdn string) (int) {
+	if strings.HasSuffix(msisdn, "+") {
+		return 1
+	}
+	if strings.HasSuffix(msisdn, "00") {
+		return 2
+	}
+	return 0
+}
+
+func find_mno_identifier(country_code string, subscriber_number string) (string) {
+	mnos := mno_identifiers[country_code]
+	for i := 1; i <= len(subscriber_number); i++ {
+		var ok bool
+		mno_identifier, ok := mnos[subscriber_number[0: i]]
+		if ok {
+			return mno_identifier
+		}
+	}
+	return "unknown"
 }
